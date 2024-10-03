3e1f5221af763d171ce7ee0da63474e0e265706c
Anders Bredgaard Thuesen <andersbthuesen@gmail.com>
2024-10-03 11:11:41 -0700

    Fix emailjs import for cinema page

--- src/sections/cinemas/Contact.tsx
+++ src/sections/cinemas/Contact.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { useForm } from "react-hook-form";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 import { useTranslation } from "react-i18next";
 const Section = styled.section`
   padding: 5rem 1rem 200px;
@@ -85,123 +85,123 @@ const SuccessMsg = styled.span`
   margin-bottom: 1rem;
 `;
 const Contact: React.FC = () => {
-  const { t } = useTranslation("cinema");
-  const { handleSubmit, register, formState: { errors } } = useForm<{
-    name: string;
-    email: string;
-    phone: string;
-    subject: string;
-    message: string;
-  }>();
-  const [loading, setLoading] = useState(false);
-  const [emailSend, setEmailSend] = useState<boolean>(false);
-  const [preventSpam, setPreventSpam] = useState<boolean>(false);
-  const onSubmit = async values => {
-    setLoading(true);
-    const expireStr = localStorage.getItem("preventSpam");
-    if (!expireStr || (expireStr && parseInt(expireStr) < Date.now())) {
-      try {
-        await emailjs.send(
-          process.env.GATSBY_EMAIL_SERVICEID,
-          "template_trrw2nh",
-          values,
-          process.env.GATSBY_EMAIL_USER
-        );
-        localStorage.setItem(
-          "preventSpam",
-          JSON.stringify(Date.now() + 600000)
-        );
-        setEmailSend(true);
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      setPreventSpam(true);
-    }
+    const { t } = useTranslation("cinema");
+    const { handleSubmit, register, formState: { errors } } = useForm<{
+        name: string;
+        email: string;
+        phone: string;
+        subject: string;
+        message: string;
+    }>();
+    const [loading, setLoading] = useState(false);
+    const [emailSend, setEmailSend] = useState<boolean>(false);
+    const [preventSpam, setPreventSpam] = useState<boolean>(false);
+    const onSubmit = async values => {
+        setLoading(true);
+        const expireStr = localStorage.getItem("preventSpam");
+        if (!expireStr || (expireStr && parseInt(expireStr) < Date.now())) {
+            try {
+                await emailjs.send(
+                    process.env.GATSBY_EMAIL_SERVICEID,
+                    "template_trrw2nh",
+                    values,
+                    // process.env.GATSBY_EMAIL_USER
+                );
+                localStorage.setItem(
+                    "preventSpam",
+                    JSON.stringify(Date.now() + 600000)
+                );
+                setEmailSend(true);
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            setPreventSpam(true);
+        }
 
-    setLoading(false);
-  };
-  return (
-    <Section>
-      <Heading>{t("contact.heading")}</Heading>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Label>
-          {t("contact.name")}
-          <Input
-            css={loading && disabledInput}
-            disabled={loading}
-            name="name"
-            {...register("name", {
-              required: t<string>("contact.fieldRequired"),
-            })}
-          />
-          <ErrorMsg>{errors.name && errors.name.message}</ErrorMsg>
-        </Label>
-        <Label>
-          {t("contact.email")}
-          <Input
-            css={loading && disabledInput}
-            disabled={loading}
-            name="email"
-            {...register("email", {
-              required: t<string>("contact.fieldRequired"),
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: t<string>("contact.emailValidation"),
-              },
-            })}
-          />
-          <ErrorMsg>{errors.email && errors.email.message}</ErrorMsg>
-        </Label>
-        <Label>
-          {t("contact.phone")}
-          <Input
-            css={loading && disabledInput}
-            disabled={loading}
-            {...register("phone", {
-              required: t<string>("contact.fieldRequired"),
-              minLength: {
-                value: 8,
-                message: t<string>("contact.phoneValidation"),
-              },
-            })}
-          />
-          <ErrorMsg>{errors.phone && errors.phone.message}</ErrorMsg>
-        </Label>
-        <Label>
-          {t("contact.subject")}
-          <Input
-            css={loading && disabledInput}
-            disabled={loading}
-            name="subject"
-            {...register("subject", {
-              required: t<string>("contact.fieldRequired"),
-            })}
-          />
-          <ErrorMsg>{errors.subject && errors.subject.message}</ErrorMsg>
-        </Label>
-        <Label>
-          {t("contact.message")}
-          <Textarea
-            css={loading && disabledInput}
-            disabled={loading}
-            name="message"
-            {...register("message", {
-              required: t<string>("contact.fieldRequired"),
-            })}
-          />
-          <ErrorMsg>{errors.message && errors.message.message}</ErrorMsg>
-        </Label>
-        {!preventSpam && emailSend && (
-          <SuccessMsg>{t("contact.success")}</SuccessMsg>
-        )}
-        {preventSpam && <ErrorMsg>{t("contact.spam")}</ErrorMsg>}
-        <SubmitButton css={loading && disabledButton}>
-          {t("contact.buttonText")}
-        </SubmitButton>
-      </Form>
-    </Section>
-  );
+        setLoading(false);
+    };
+    return (
+        <Section>
+            <Heading>{t("contact.heading")}</Heading>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Label>
+                    {t("contact.name")}
+                    <Input
+                        css={loading && disabledInput}
+                        disabled={loading}
+                        name="name"
+                        {...register("name", {
+                            required: t<string>("contact.fieldRequired"),
+                        })}
+                    />
+                    <ErrorMsg>{errors.name && errors.name.message}</ErrorMsg>
+                </Label>
+                <Label>
+                    {t("contact.email")}
+                    <Input
+                        css={loading && disabledInput}
+                        disabled={loading}
+                        name="email"
+                        {...register("email", {
+                            required: t<string>("contact.fieldRequired"),
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: t<string>("contact.emailValidation"),
+                            },
+                        })}
+                    />
+                    <ErrorMsg>{errors.email && errors.email.message}</ErrorMsg>
+                </Label>
+                <Label>
+                    {t("contact.phone")}
+                    <Input
+                        css={loading && disabledInput}
+                        disabled={loading}
+                        {...register("phone", {
+                            required: t<string>("contact.fieldRequired"),
+                            minLength: {
+                                value: 8,
+                                message: t<string>("contact.phoneValidation"),
+                            },
+                        })}
+                    />
+                    <ErrorMsg>{errors.phone && errors.phone.message}</ErrorMsg>
+                </Label>
+                <Label>
+                    {t("contact.subject")}
+                    <Input
+                        css={loading && disabledInput}
+                        disabled={loading}
+                        name="subject"
+                        {...register("subject", {
+                            required: t<string>("contact.fieldRequired"),
+                        })}
+                    />
+                    <ErrorMsg>{errors.subject && errors.subject.message}</ErrorMsg>
+                </Label>
+                <Label>
+                    {t("contact.message")}
+                    <Textarea
+                        css={loading && disabledInput}
+                        disabled={loading}
+                        name="message"
+                        {...register("message", {
+                            required: t<string>("contact.fieldRequired"),
+                        })}
+                    />
+                    <ErrorMsg>{errors.message && errors.message.message}</ErrorMsg>
+                </Label>
+                {!preventSpam && emailSend && (
+                    <SuccessMsg>{t("contact.success")}</SuccessMsg>
+                )}
+                {preventSpam && <ErrorMsg>{t("contact.spam")}</ErrorMsg>}
+                <SubmitButton css={loading && disabledButton}>
+                    {t("contact.buttonText")}
+                </SubmitButton>
+            </Form>
+        </Section>
+    );
 };
 
 export default Contact;
