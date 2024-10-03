50e8a2f811f6c5fd7997a9654175cd83aacde56f
Anders Bredgaard Thuesen <andersbthuesen@gmail.com>
2024-10-03 11:21:46 -0700

    Try again

--- src/sections/schools/Contact.tsx
+++ src/sections/schools/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { useForm } from "react-hook-form";
 import emailjs from "@emailjs/browser";
