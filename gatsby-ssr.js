26d172c9c6e3ad7fd3e922b30315f8de90ba83d2
alexandersubreader <83945889+alexandersubreader@users.noreply.github.com>
2026-02-23 23:42:47 +0100

    Add Google Analytics 4 integration in gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -34,3 +34,49 @@ export const onRenderBody = ({ setHeadComponents }) => {
     ]);
   }
 };
+
+const GA_ID = "G-F86HKGDW1H";
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    // 1) Consent Mode defaults (deny until user decides)
+    <script
+      key="consent-default"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+
+          gtag('consent', 'default', {
+            ad_storage: 'denied',
+            analytics_storage: 'denied',
+            ad_user_data: 'denied',
+            ad_personalization: 'denied',
+            functionality_storage: 'denied',
+            personalization_storage: 'denied',
+            security_storage: 'granted'
+          });
+
+          // Optional but recommended: reduce/obfuscate ads data until consent
+          gtag('set', 'ads_data_redaction', true);
+        `,
+      }}
+    />,
+
+    // 2) Load GA4 (gtag.js)
+    <script async key="gtag-src" src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />,
+
+    // 3) Init GA4
+    <script
+      key="gtag-init"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}', { anonymize_ip: true });
+        `,
+      }}
+    />,
+  ]);
+};
